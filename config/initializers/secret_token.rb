3100ee3ac90ac5c9445b45313788217d1a9b3f0b
Pavel Smolka <pavel.smolka@gmail.com>
2013-01-30 21:56:50 +0100

    Generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roommating::Application.config.secret_token = 'b32c6a41ffe0c45cb2115b0e2f1517262f6bff074b76128b772e07b8408a1b95ef8166423667c47827fea73297703208cf05b51ed44d4d25891992cb30be6edd'
